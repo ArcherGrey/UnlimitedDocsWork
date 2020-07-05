25657883bbf800a06b7880e48accb46f3af0e983
ArcherGrey <498792539@qq.com>
2020-07-05 23:28:52 +0800

    test

--- test/test.js
+++ test/test.js
@@ -1,40 +1,7 @@
 /**
- * @param {number[]} nums1
- * @param {number[]} nums2
- * @return {number[]}
+ * @param {string} s
+ * @param {string} p
+ * @return {boolean}
  */
-function bs(nums, target) {
-  let l = 0,
-    r = nums.length - 1;
-  while (l <= r) {
-    let mid = ~~((l + r) / 2);
-    if (nums[mid] > target) {
-      r = mid - 1;
-    } else if (nums[mid] < target) {
-      l = mid + 1;
-    } else {
-      return mid;
-    }
-  }
-  return -1;
-}
-var intersection = function (nums1, nums2) {
-  // 先排序
-  nums1.sort((a, b) => {
-    return a - b;
-  });
-  nums2.sort((a, b) => {
-    return a - b;
-  });
-
-  let r = [];
-  // 遍历数组1
-  for (var i = 0; i < nums1.length; i++) {
-    // 在数组2中用二分查找，查找是否存在当前值
-    if (bs(nums2, nums1[i]) > -1) {
-      // 如果存在，再在结果数组中查找是否已经添加
-      if (bs(r, nums1[i]) == -1) r.push(nums1[i]);
-    }
-  }
-  return r;
-};
+var isMatch = function (s, p) {};
+xxx;
