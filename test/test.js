5ec6746b3068623b99393e7a2148fe6ef77f9869
ArcherGrey <498792539@qq.com>
2020-08-21 19:26:58 +0800

    docs: update

--- test/test.js
+++ test/test.js
@@ -1,17 +1,20 @@
 /**
- * @param {number[]} nums
- * @return {number}
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
  */
-var singleNumber = function(nums) {
-  let s = new Set();
-  for (let i = 0; i < nums.length; ++i) {
-    // 除了一个每个都出现了两次
-    // 那就先加入再刪除
-    // 最后剩下的就是所求
-    if (!s.has(nums[i])) s.add(nums[i]);
-    else s.delete(nums[i]);
+var intersection = function(nums1, nums2) {
+  // 哈希集合1保存数组1去重后的结果
+  // 哈希集合2保存哈希集合1和数组2去重后的结果
+  // 哈希集合2 转换为数组返回
+  let s1 = new Set(),
+    s2 = new Set(),
+  ans = [];
+  for (let i = 0; i < nums1.length; ++i) {
+    s1.add(nums1[i]);
   }
-  for (let x of s) {
-    return x;
+  for (let i = 0; i < nums2.length; ++i) {
+    if (s1.has(nums2[i])) s2.add(nums2[i])
   }
+  for(let e of)
 };
