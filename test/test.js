d003ecf35b3a6bb31a05270e5f7af93287f301ca
ArcherGrey <498792539@qq.com>
2020-08-25 18:24:23 +0800

    docs: update

--- test/test.js
+++ test/test.js
@@ -1,36 +1,50 @@
 /**
- * @param {number[]} nums
- * @return {number[][]}
+ * @param {string} s
+ * @return {boolean}
  */
-var findSubsequences = function(nums) {
-  let tmp = [],
-    ans = [];
-  /**
-   * @description:
-   * @param {number} cur 当前下标
-   * @param {number} last 最近的值
-   * @return {type}
-   */
-  function dfs(cur, last, nums) {
-    // 遍历完成
-    if (cur == nums.length) {
-      // 满足长度大于等于2加入结果
-      if (tmp.length >= 2) {
-        ans.push([...tmp]);
-      }
-      return;
+var repeatedSubstringPattern = function(s) {
+  return kmp(s + s, s);
+};
+
+/**
+ * @description: kmp 算法
+ * @param {string} q 主串
+ * @param {string} p 模式串
+ * @return {type}
+ */
+function kmp(q, p) {
+  let n = q.length,
+    m = p.length;
+
+  // 求 next 数组
+  let next = new Array(m).fill(-1);
+  for (let i = 1; i < m; ++i) {
+    // 前面子串有几个重复的
+    let j = next[i - 1];
+    // 不相等就缩小范围
+    while (j != -1 && p[j + 1] != p[i]) {
+      j = next[j];
     }
-    // 满足条件加入 更新 cur last
-    if (nums[cur] >= last) {
-      tmp.push(nums[cur]);
-      dfs(cur + 1, nums[cur], nums);
-      tmp.pop();
-    }
-    // 不满足条件继续
-    if (nums[cur] != last) {
-      dfs(cur + 1, last, nums);
+    // 相等就加1
+    if (p[j + 1] == p[i]) {
+      next[i] = j + 1;
     }
   }
-  dfs(0, -101, nums);
-  return ans;
-};
+
+  // 模式串中的位置
+  let match = -1;
+
+  // 遍历主串
+  for (let i = 1; i < n - 1; ++i) {
+    while (match != -1 && p[match + 1] != q[i]) {
+      match = next[match];
+    }
+    if (p[match + 1] == q[i]) {
+      ++match;
+      if (match == m - 1) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
